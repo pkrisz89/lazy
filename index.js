7357cb0d17c06d7ca01b8752a3907b5fe41eee2d
Kris Pinter <pinter.krisztian89@gmail.com>
2019-01-31 18:36:30 +0000

    Create evaluate method

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 module.exports = class Lazy {
   constructor() {
-    this.args = new Map();
+    this.dataMap = new Map();
   }
 
   add(fn, ...args) {
-    this.args.set(fn, [...args]);
+    this.dataMap.set(fn, [...args]);
     return this;
+  }
+
+  evaluate(target) {
+    return target.map(val => {
+      let result;
+
+      for (let [fn, args] of this.dataMap) {
+        result = !result
+          ? fn.apply(this, [...args, val])
+          : fn.apply(this, [...args, result]);
+      }
+
+      return result;
+    });
   }
 };
--- test.js
+++ test.js
@@ -20,18 +20,37 @@ describe("Lazy", () => {
     it("should save the fn and the arguments as k-v pairs", () => {
       computation.add(timesTwo);
 
-      expect(computation.args.has(timesTwo)).toBe(true);
-      expect(computation.args.get(timesTwo)).toEqual([]);
+      expect(computation.dataMap.has(timesTwo)).toBe(true);
+      expect(computation.dataMap.get(timesTwo)).toEqual([]);
     });
 
     it("should be chainable", () => {
       computation.add(timesTwo).add(plus, 1, 2, 3);
 
-      expect(computation.args.has(timesTwo)).toBe(true);
-      expect(computation.args.get(timesTwo)).toEqual([]);
+      expect(computation.dataMap.has(timesTwo)).toBe(true);
+      expect(computation.dataMap.get(timesTwo)).toEqual([]);
 
-      expect(computation.args.has(plus)).toBe(true);
-      expect(computation.args.get(plus)).toEqual([1, 2, 3]);
+      expect(computation.dataMap.has(plus)).toBe(true);
+      expect(computation.dataMap.get(plus)).toEqual([1, 2, 3]);
+    });
+  });
+
+  describe("Evaluate method", () => {
+    it("should return a result array (one function)", () => {
+      const expected = [4];
+      const result = computation.add(timesTwo).evaluate([2]);
+
+      expect(result).toEqual(expected);
+    });
+
+    it("should return a result array (multiple functions)", () => {
+      const expected = [3, 5, 7];
+      const result = computation
+        .add(timesTwo)
+        .add(plus, 1)
+        .evaluate([1, 2, 3]);
+
+      expect(result).toEqual(expected);
     });
   });
 });
