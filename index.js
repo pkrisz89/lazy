1d5ab743c610214526adcde60cd206695ddea894
Kris Pinter <pinter.krisztian89@gmail.com>
2019-01-31 18:01:01 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = class Lazy {
+  constructor() {}
+
+  add(...args) {}
+};
---
+++ test.js
@@ -0,0 +1,7 @@
+let expect = require("expect");
+
+describe("Lazy", () => {
+  it("should work", () => {
+    expect(1).toEqual(1);
+  });
+});
